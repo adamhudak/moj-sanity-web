9516c2574500f8b7ea62fc75b8d1fa5b592d3985
adamhudak <adytosbeats@gmail.com>
2026-01-25 20:29:53 +0100

    add new about page

---
+++ schemaTypes/aboutPage.ts
@@ -0,0 +1,42 @@
+export default {
+  name: 'aboutPage',
+  type: 'document',
+  title: 'O nás',
+  fields: [
+    { name: 'title', type: 'string', title: 'Nadpis stránky' },
+    {
+      name: 'sections',
+      type: 'array',
+      title: 'Sekcie stránky',
+      of: [
+        {
+          type: 'object',
+          name: 'heroSection',
+          title: 'Úvodná sekcia (Obrázok + Text)',
+          fields: [
+            { name: 'image', type: 'image', title: 'Obrázok' },
+            { name: 'text', type: 'text', title: 'Text' }
+          ]
+        },
+        {
+          type: 'object',
+          name: 'faqSection',
+          title: 'Akordeón (FAQ)',
+          fields: [
+            {
+              name: 'questions',
+              type: 'array',
+              of: [{
+                type: 'object',
+                fields: [
+                  { name: 'question', type: 'string', title: 'Otázka' },
+                  { name: 'answer', type: 'text', title: 'Odpoveď' }
+                ]
+              }]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import { postType } from './postType'
 import { categoryType } from './categoryType'
+import aboutPage from './aboutPage'
 
-export const schemaTypes = [postType, categoryType]
+export const schemaTypes = [postType, categoryType, aboutPage]
